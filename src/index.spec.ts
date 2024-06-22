b02c793560096d0b2d63bd1ef29f549524b9715f
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2024-06-22 21:42:59 +0530

    test: added

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,1 +1,9 @@
 import { describe, expect, it } from "vitest";
+
+describe("String calculator", () => {
+  it.todo("should return zero on empty input string");
+  it.todo("should return the same value as input if no other number");
+  it.todo(
+    "should return sum of all the numbers in input string (comma separated)"
+  );
+});
