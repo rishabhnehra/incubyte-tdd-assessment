c637e012718f280a2c3210690bfb4f3613ae3157
Rishabh Nehra <rishabhnehrapersonal@gmail.com>
2024-06-22 21:48:30 +0530

    test: implementation

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,22 @@
 import { describe, expect, it } from "vitest";
+import { stringCalculator } from "./string-calculator";
 
 describe("String calculator", () => {
-  it.todo("should return zero on empty input string");
-  it.todo("should return the same value as input if no other number");
-  it.todo(
-    "should return sum of all the numbers in input string (comma separated)"
-  );
+  it("should return zero on empty input string", () => {
+    const result = stringCalculator("");
+
+    expect(result).toBe(0);
+  });
+
+  it("should return the same value as input if no other number", () => {
+    const result = stringCalculator("1");
+
+    expect(result).toBe(1);
+  });
+
+  it("should return sum of all the numbers in input string (comma separated)", () => {
+    const result = stringCalculator("1,5");
+
+    expect(result).toBe(6);
+  });
 });
